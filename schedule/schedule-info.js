99a9bea3bd2a1ce3bb19d9d725348f75096ffb5b
yukio yamamoto <aj.yamamoto@me.com>
2016-09-07 21:12:00 -1000

    Modifying path from /core to /JDLA-Web-Development

--- schedule/schedule-info.js
+++ schedule/schedule-info.js
@@ -1,4 +1,4 @@
 moreaEventData = [
-  {title: "Experience the Baz", url: "/core/morea/baz/experience.html", start: "2015-06-11T18:00"},
-  {title: "Learn to Baz", url: "/core/modules/baz", start: "2015-06-11", end: "2015-06-15"}
+  {title: "Experience the Baz", url: "/JDLA-Web-Development/morea/baz/experience.html", start: "2015-06-11T18:00"},
+  {title: "Learn to Baz", url: "/JDLA-Web-Development/modules/baz", start: "2015-06-11", end: "2015-06-15"}
 ]
