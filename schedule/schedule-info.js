5e43fb8886b9916200f1e040a0c59da5211e8457
yukio yamamoto <aj.yamamoto@me.com>
2016-09-07 21:07:36 -1000

    Reverting to original _config.xml

--- schedule/schedule-info.js
+++ schedule/schedule-info.js
@@ -1,4 +1,4 @@
 moreaEventData = [
-  {title: "Experience the Baz", url: "/morea/baz/experience.html", start: "2015-06-11T18:00"},
-  {title: "Learn to Baz", url: "/modules/baz", start: "2015-06-11", end: "2015-06-15"}
+  {title: "Experience the Baz", url: "/core/morea/baz/experience.html", start: "2015-06-11T18:00"},
+  {title: "Learn to Baz", url: "/core/modules/baz", start: "2015-06-11", end: "2015-06-15"}
 ]
